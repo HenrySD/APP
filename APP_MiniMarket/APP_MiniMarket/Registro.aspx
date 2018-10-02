90cc14b72eaa0782d6f979570648f23206febca9
mateoportillo19 <mateo.alex47@gmail.com>
2018-10-02 11:02:26 -0600

    Registro

---
+++ APP_MiniMarket/APP_MiniMarket/Registro.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registro.aspx.cs" Inherits="APP_MiniMarket.Registro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Registro Minimarket</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap">
+    </head>
+<body>
+      <style type="text/css">
+        .auto-style1 {
+            margin-top: 0;
+            margin-bottom: 0;
+            font-size: 16px;
+            color: inherit;
+            text-align: center;
+        }
+       body{
+    background-color: #07989c;
+}
+.centered-form{
+	margin-top: 60px;
+}
+
+.centered-form .panel{
+	background: rgba(255, 255, 255, 0.8);
+	box-shadow: rgba(0, 0, 0, 0.3) 20px 20px 20px;
+}
+    </style>
+    <div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default">
+        		<div class="panel-heading">
+                   
+                        <h3 class="auto-style1"> Registro De Usuario</h3>
+			 			</div>
+                 
+			 			<div class="panel-body">
+			    		<form role="form">
+			    			<div class="row">
+
+                                    <form id="form1" runat="server">
+                                    <div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                                      <asp:TextBox ID="TxtNombre" class="form-control input-sm" placeholder="Nombre" runat="server"></asp:TextBox>
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                                      <asp:TextBox ID="TxtApellido"  class="form-control input-sm" placeholder="Apellido" runat="server"></asp:TextBox>
+			    					</div>
+			    				</div>
+			    			</div>
+                            <div class="col-xs-6 col-sm-6 col-md-12">
+			    			<div class="form-group">
+                                <asp:TextBox ID="TxtEmail"  class="form-control input-sm" placeholder="Email" runat="server" TextMode="Email"></asp:TextBox>
+			    			</div>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                                        <asp:TextBox ID="TxtCPassword" class="form-control input-sm" placeholder="Contraseña" runat="server" TextMode="Password"></asp:TextBox>
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+                                       <asp:TextBox ID="TxtComfirPass" class="form-control input-sm" placeholder="Confirmar Contraseña" runat="server" TextMode="Password"></asp:TextBox>
+			    					</div>
+			    				</div>
+                                <div class="col-xs-6 col-sm-6 col-md-12">
+                                <div class="form-group">
+                                <asp:TextBox ID="TxtDireccion" class="form-control input-sm" placeholder="Direccion" runat="server"></asp:TextBox>
+			    			</div>
+                                   
+                                <div class="form-group">
+                                <asp:TextBox ID="TxtCalle" class="form-control input-sm" placeholder="Calle"  runat="server"></asp:TextBox>
+			    			</div>
+                                 
+                                <div class="form-group">
+                                 <asp:TextBox ID="TxtNumCasa" class="form-control input-sm" placeholder="Numero de Casa" runat="server" TextMode="Phone"></asp:TextBox>
+			    			</div>
+                                    <div class="form-group">
+                                  <asp:TextBox ID="TxtBarrio" class="form-control input-sm" placeholder="Barrio" runat="server"></asp:TextBox>                       
+			    			</div>
+			    			</div>
+                              &nbsp;</div>
+                            <asp:Button ID="btnRegistrar" class="btn btn-info btn-block" Text="Registrar" runat="server" OnClick="btnRegistrar_Click" />
+			    			
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>   
+    </div>
+    </form>
+</body>
+</html>
---
+++ APP_MiniMarket/APP_MiniMarket/Registro.aspx.designer.cs
@@ -0,0 +1,123 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace APP_MiniMarket {
+    
+    
+    public partial class Registro {
+        
+        /// <summary>
+        /// Control bootstrap.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink bootstrap;
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control TxtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNombre;
+        
+        /// <summary>
+        /// Control TxtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtApellido;
+        
+        /// <summary>
+        /// Control TxtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtEmail;
+        
+        /// <summary>
+        /// Control TxtCPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtCPassword;
+        
+        /// <summary>
+        /// Control TxtComfirPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtComfirPass;
+        
+        /// <summary>
+        /// Control TxtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtDireccion;
+        
+        /// <summary>
+        /// Control TxtCalle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtCalle;
+        
+        /// <summary>
+        /// Control TxtNumCasa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtNumCasa;
+        
+        /// <summary>
+        /// Control TxtBarrio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtBarrio;
+        
+        /// <summary>
+        /// Control btnRegistrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegistrar;
+    }
+}
